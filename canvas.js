680869ebfc20d15e835e3a412dad5dd3c4c5e0a6
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2015-02-01 14:59:42 +0000

    Bla

--- canvas.js
+++ canvas.js
@@ -1,13 +1,47 @@
-var Canvas = Base.extend({
-    constructor: function() {
-        this.canvas = $('#Canvas')[0];
-        this.context = this.canvas.getContext('2d');
-    },
-    canvas: undefined,
-    context: undefined
-});
+function Canvas() {
+    this.canvas = $('#Canvas')[0];
+    this.context = this.canvas.getContext('2d');
+    this.shapes = [];
+};
 
 var can = new Canvas();
+
+function Shape(x,y) {
+    
+}
+
+function Pen(x,y) {
+    this.x = [x];
+    this.y = [y];
+};
+
+Pen.prototype = new Shape();
+
+Pen.prototype.draw = function(canvas) {
+    canvas.context.beginPath();
+    canvas.context.moveTo(this.x[0],this.y[0]);
+    for (var i = 1; i < this.x.length; ++i) {
+        canvas.context.lineTo(this.x[i],this.y[i]);
+    }
+    canvas.context.stroke();
+};
+
+var pen = new Pen(10,15);
+pen.x.push(100);
+pen.y.push(50);
+pen.x.push(400);
+pen.y.push(70);
+pen.draw(can);
+
+var pen2 = new Pen(50,60);
+pen2.x.push(600);
+pen2.y.push(500);
+pen2.x.push(30);
+pen2.y.push(80);
+pen2.draw(can);
+
+console.log(pen.x);
+console.log(pen2.x);
 
 var x1 = 100;
 var y1 = 150;
@@ -18,6 +52,7 @@ for (var i = 0; i < 10; i++) {
     can.context.beginPath();
     can.context.moveTo(x1, y1);
     can.context.lineTo(x2, y2);
+    can.context.lineTo(300,400);
     can.context.stroke();
     x1 = x1 + 10;
     y1 = y1 + 10;
